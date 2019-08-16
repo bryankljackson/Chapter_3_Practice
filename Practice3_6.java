9de44314f6e62979d3104fed49b8d6ac81bb8248
bryankljackson <47409745+bryankljackson@users.noreply.github.com>
2019-08-16 13:01:17 -0400

    Add files via upload

---
+++ Practice3_6.java
@@ -0,0 +1,46 @@
+/*
+ * Write an if statement that increases pay by 3% if score is greater 
+ * than 90, otherwise increases pay by 1%.
+*/
+
+package demo;
+import java.util.Scanner;
+
+public class Practice3_6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		
+		//Prompt the user
+		
+		System.out.println("Please input score: ");
+		double score = input.nextDouble();
+		
+		if (score > 90) {
+			System.out.println("Please input current pay: $");
+			double originalPay = input.nextDouble();
+			
+			double newPay;
+			newPay = originalPay * 1.03;
+			System.out.println("Your original pay was " + 
+					originalPay + " and your new pay is $" + newPay);
+		}
+		
+		else {
+			System.out.println("Please input current pay: $");
+			double originalPay = input.nextDouble();
+			
+			double newPay;
+			newPay = originalPay * 1.01;
+			System.out.println("Your original pay was " + 
+					originalPay + " and your new pay is $" + newPay);
+			
+		}
+		
+		
+	}
+	
+
+}
