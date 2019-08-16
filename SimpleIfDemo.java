3d72f08929e9047bc1a5171935f793a22eed143d
bryankljackson <47409745+bryankljackson@users.noreply.github.com>
2019-08-16 12:14:37 -0400

    Update SimpleIfDemo.java

--- SimpleIfDemo.java
+++ SimpleIfDemo.java
@@ -1,1 +1,27 @@
+package demo;
+import java.util.Scanner;
 
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Insert an integer: ");
+		int number = input.nextInt();
+		
+		if (number % 5 == 0) {
+			System.out.println("HiFive");
+		}
+		
+		if (number % 2 == 0) {
+			System.out.println("HiEven");
+		}
+		
+		
+		
+		
+	}
+
+}
